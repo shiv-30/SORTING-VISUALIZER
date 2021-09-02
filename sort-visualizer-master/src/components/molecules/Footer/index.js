11371a432c4ed188b303d34603371814f82174e9
Shiv-30 <sosmart08642@gmail.com>
2021-09-02 10:03:42 +0530

    first Commit

---
+++ sort-visualizer-master/src/components/molecules/Footer/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer" >
+      <section>
+        Designed by {' '}
+        <span className="Footer__Heart">&hearts;</span> by{' '}
+        
+          Shivam Kumar
+      
+      </section>
+
+      <section className="Footer__Items">
+        
+         
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
